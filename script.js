45241a2af169bdab081ff7950c1f434f8fceb996
Andi Devois <devoisaurus@Andi-Devoiss-MacBook-Pro.local>
2016-02-27 13:17:56 -0600

    initial js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-var languageTranslator = {};
+var userInput = document.getElementById("input").value;
+var button = document.getElementById("button");
+var output = document.getElementById("output");
+var splitInput = userInput.split(" ");
+
+
+// function returnText(userInput){
+// 	var splitInput = userInput.split(" ");
+// 	return splitInput;
+// 	console.log(returnText);
+// }
+
+var languageTranslator = (function() {
+	if(document.getElementById("dropdown").value == welsh) {
+		languageTranslator.translateToWelsh();
+	}
+}
+
+button.addEventListener("click", languageTranslator(function){
+
+}));
